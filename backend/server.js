fa2108e5fc81a4c0f0913ad65e8f5576464266b7
iwom <17498068+iwom@users.noreply.github.com>
2018-11-02 15:38:05 +0100

    Test POST request, added body-parser

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 const db = require('./models/index');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 
 const app = express();
+app.use(bodyParser.json());
+
+
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+    res.send('Hello world\n');
+});
+
+app.post('/test-post', (req, res) => {
+    console.log(req.body);
+    const obj = {
+        test: "Test"
+    };
+    res.status(201).send(obj);
 });
 
 
