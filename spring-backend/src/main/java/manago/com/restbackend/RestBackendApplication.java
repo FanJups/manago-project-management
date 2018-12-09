7386bb60054a975775e43c5f52f6f5d7aaf456ae
iwom <17498068+iwom@users.noreply.github.com>
2018-12-09 12:37:58 +0100

    Removed node.js

---
+++ spring-backend/src/main/java/manago/com/restbackend/RestBackendApplication.java
@@ -0,0 +1,12 @@
+package manago.com.restbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestBackendApplication.class, args);
+    }
+}
