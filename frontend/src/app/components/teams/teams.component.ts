68fe5e3eecedb853bdbfe85765c69305d659d814
iwom <17498068+iwom@users.noreply.github.com>
2019-01-05 12:06:04 +0100

    /teams

--- frontend/src/app/components/teams/teams.component.ts
+++ frontend/src/app/components/teams/teams.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatSnackBar, MatTableDataSource} from '@angular/material';
+import {Team} from '../../models/team';
+import {TeamService} from '../../services/team.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-teams',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teams.component.css']
 })
 export class TeamsComponent implements OnInit {
+  teams: MatTableDataSource<Team> = new MatTableDataSource();
+  displayedColumns = ['name', 'size', 'monthlyCost', 'detail', 'edit', 'delete'];
 
-  constructor() { }
+  constructor(
+    private teamService: TeamService,
+    private router: Router,
+    public dialog: MatDialog,
+    private snackbar: MatSnackBar
+  ) { }
 
   ngOnInit() {
+    this.getTeams();
+  }
+
+  getTeams(): void {
+    this.teamService.getTeams().subscribe(resp => {
+      this.teams.data = resp;
+    });
   }
 
 }
--- frontend/src/app/services/team.service.ts
+++ frontend/src/app/services/team.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AppEndpoints} from './app-endpoints.service';
+import {Observable} from 'rxjs/Observable';
+import {Team} from '../models/team';
 
 @Injectable()
 export class TeamService {
@@ -9,5 +11,9 @@ export class TeamService {
     private httpClient: HttpClient,
     private appEndpoints: AppEndpoints
   ) {
+  }
+
+  getTeams(): Observable<Team[]> {
+    return this.httpClient.get<Team[]>(this.appEndpoints.go().teams(), {observe: 'body'});
   }
 }
